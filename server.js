41a3593bc11ebbc5c051545c09347d0e80eb8867
vladimir shamsheev <shamsheev@MacBook-Pro-vladimir-2.local>
2016-01-09 19:36:08 +0300

    add_env_log

--- server.js
+++ server.js
@@ -6,5 +6,6 @@ var argv = require('yargs').argv;
 var assignSafe = require('./assign-safe.js');
 
 assignSafe(process.env, JSON.parse(argv.exchangeData));
+console.log('process.env:', process.env);
 
 impress.server.start();
